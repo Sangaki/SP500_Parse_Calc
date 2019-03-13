b2456e0345fca28ea34b3a2d4ed2cfe763c1c6fd
daemonica <dima62131@aol.com>
2019-03-13 21:18:58 +0300

    test diff

---
+++ asdasd.py
@@ -0,0 +1,27 @@
+import math
+import datas
+import datetime
+
+
+def my_function(otrezok):
+    x = []
+    out = []
+    for i in range(0, len(otrezok)-1):
+        x[i][1] = math.fabs(math.log(float(otrezok[i+1][1])) - math.log(float(otrezok[i][1])))
+    for i in range(0, len(x)):
+        out[i][1] = x[i][1] / math.fsum(x)
+    print(out)
+
+
+start = datetime.datetime.strptime("2006-1-1", "%Y-%m-%d")  # здесь мы генерим массив дат
+end = datetime.datetime.strptime("2016-1-1", "%Y-%m-%d")  # формат во второй переменной ГГГГ-ММ-ДД
+
+df = datas.get_data()
+
+results = datas.data_to_cvs(df)
+print(results)
+
+rez = datas.filling_empty_days(results, start, end)
+print(rez)
+otrez = datas.sequence(rez, start)[0]
+my_function(otrez)
