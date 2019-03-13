ae7703135c8c1c2b1f34a0431c23ded9c4ff3a25
daemonica <dima62131@aol.com>
2019-03-14 02:39:20 +0300

    asdasdXcfwetsfd frhdgfg hykjdhgfdcx

--- asdasd.py
+++ asdasd.py
@@ -1,42 +1,17 @@
 import math
 import datas
-import datetime
-import graphs
 
 
 def diff_timeline(otrezok, start, rez):
     x = otrezok
     out = x
-    print('lulkappa', type(otrezok[len(otrezok)-1][1]))
     for i in range(0, len(otrezok)-1):
         x[i][1] = math.fabs(math.log(otrezok[i+1][1]) - math.log(otrezok[i][1]))
     temp = otrezok[-1][1]
-    print(temp, type(temp))
     x[-1][1] = math.fabs(math.log(datas.sequence(rez, start)[1][0][1]) - math.log(temp))
-    print(x[-1])
     sum_of_x = 0
     for i in range(0, len(x)):
         sum_of_x += float(x[i][1])
     for i in range(0, len(x)):
         out[i][1] = float(x[i][1]) / sum_of_x
-    print('its out)')
-    print(out)
-    print(sum_of_x)
-
     return out
-
-
-# start = datetime.datetime.strptime("2006-1-1", "%Y-%m-%d")
-# end = datetime.datetime.strptime("2016-1-1", "%Y-%m-%d")
-#
-# df = datas.get_data()
-#
-# results = datas.data_to_cvs(df)
-# # print(results)
-#
-# rez = datas.filling_empty_days(results, start, end)
-# # print(rez)
-# otrez = datas.sequence(rez, start)[0]
-# out = diff_timeline(otrez)
-#
-# graphs.plot(out)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import datas
 import graphs
 import asdasd
 import datetime
+import math
 
 start = datetime.datetime.strptime("2006-1-1", "%Y-%m-%d")  # здесь мы генерим массив дат
 end = datetime.datetime.strptime("2016-1-1", "%Y-%m-%d")  # формат во второй переменной ГГГГ-ММ-ДД
@@ -9,16 +10,41 @@ end = datetime.datetime.strptime("2016-1-1", "%Y-%m-%d")  # формат во в
 df = datas.get_data()
 
 results = datas.data_to_cvs(df)
-print(results)
+#  print(results)
 
 rez = datas.filling_empty_days(results, start, end)
-print(rez)
+#  print(rez)
 
 rez = datas.floating_mass(rez)
 
 new_rez = datas.sequence(rez, start)
-print(new_rez[0][0][1])  # итоговый массив формата [интервал][дата][значение]
+# print(new_rez[0][0][1])  # итоговый массив формата [интервал][дата][значение]
 
 out = asdasd.diff_timeline(new_rez[0], start, rez)
 
 # graphs.plot(new_rez[0])
+print(out)
+
+summ = 0
+for i in range(0, len(rez)):
+    summ += rez[i][1]
+summ /= len(rez)  # среднее арифметическое ВСЕХ данных (всех отрезков)
+summ_i = 0
+square_temp = 0
+print(new_rez[0])
+qwe = 0
+for i in range(0, 60):
+    leng = len(new_rez[qwe]) - 1
+    for j in range(0, leng):
+        summ_i += new_rez[i][j][1]
+    square_temp += math.pow(summ_i, 2)  # скобочка с квадратом в сумме хуйня ебаная блять я запутался
+    summ_i = 0
+    qwe += 1
+
+print(square_temp)
+n = 60
+s = math.sqrt(square_temp/n)
+print(s)
+# alpha = 1.5708
+# h = math.log(r/s)/math.log(alpha*n)
+
