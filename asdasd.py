5a128d5f0554848d7bcb096a0878cc7b20a2e5cf
Sangaki <poirtain@mail.ru>
2019-03-13 23:19:42 +0300

    added graphs (need fix y axis)

--- asdasd.py
+++ asdasd.py
@@ -1,10 +1,11 @@
 import math
 import datas
 import datetime
+import graphs
 
 
 def my_function(otrezok):
-    x = otrez
+    x = otrezok
     out = x
     for i in range(0, len(otrezok)-1):
         x[i][1] = math.fabs(math.log(float(otrezok[i+1][1])) - math.log(float(otrezok[i][1])))
@@ -15,6 +16,7 @@ def my_function(otrezok):
         out[i][1] = float(x[i][1]) / sum_of_x
     print('its out)')
     print(out)
+    return out
 
 
 start = datetime.datetime.strptime("2006-1-1", "%Y-%m-%d")
@@ -28,5 +30,6 @@ results = datas.data_to_cvs(df)
 rez = datas.filling_empty_days(results, start, end)
 # print(rez)
 otrez = datas.sequence(rez, start)[0]
-my_function(otrez)
+out = my_function(otrez)
 
+graphs.plot(out)
