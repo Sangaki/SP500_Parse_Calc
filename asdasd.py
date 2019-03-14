4729e0e33083cada79a38a00ca2e32712960b0aa
daemonica <dima62131@aol.com>
2019-03-14 17:10:21 +0300

    asdasd actually fixed

--- asdasd.py
+++ asdasd.py
@@ -2,10 +2,13 @@ import math
 
 
 def diff_timeline(otrezok):
-    x = otrezok
-    out = x
-    for i in range(0, len(otrezok)-1):
-        x[i][1] = math.fabs(math.log(otrezok[i+1][1]) - math.log(otrezok[i][1]))
+    x = [[0] * 2 for i in range(len(otrezok))]
+    out = [[0] * 2 for i in range(len(otrezok))]
+    for i in range(len(otrezok)):
+        x[i][0] = otrezok[i][0]
+        out[i][0] = otrezok[i][0]
+    for i in range(len(otrezok)-1):
+        x[i][1] = math.fabs(math.log(float(otrezok[i+1][1])) - math.log(float(otrezok[i][1])))
     sum_of_x = 0
     for i in range(0, len(x)):
         sum_of_x += float(x[i][1])
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import datas
-import graphs
+# import graphs
 import asdasd
 import datetime
 import math
@@ -10,16 +10,12 @@ end = datetime.datetime.strptime("2016-1-1", "%Y-%m-%d")  # формат во в
 df = datas.get_data()
 
 results = datas.data_to_cvs(df)
-#  print(results)
 
 rez = datas.filling_empty_days(results, start, end)
-#  print(rez)
 
 rez = datas.floating_mass(rez)
 new_rez = datas.sequence(rez, start)
 out = asdasd.diff_timeline(rez)
-# TODO ПОЧЕМУ ПОСЛЕ ВЫПОЛНЕНИЯ ЭТОЙ ФУНКЦИИ В new_rez НА ПЕРВОМ ИНТЕРВАЛЕ ДИФФЕРЕНЦИРОВАННЫЕ ЗНАЧЕНИЯ???????
-
 # graphs.plot(new_rez[0])
 
 summ = 0
@@ -43,10 +39,12 @@ for each in range(len(new_rez)):
 s = math.sqrt(square_temp/len(new_rez))
 alpha = 1.5708  # по той формуле можно альфу на 0.5 поменять и тогда h=0.4011
 r = max(z) - min(z)
-h = math.log(r/s)/math.log(0.5*alpha*len(new_rez))
-
-# print('H = ', h)
+h = math.log(r/s)/math.log(alpha*len(new_rez))
+print('R = ', r)
+print('S = ', s)
+print('Z = ', z)
+print('H = ', h)
 
 k = 1.3 + h
 
-# print('k = ', k)
+print('k = ', k)
