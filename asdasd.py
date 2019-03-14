4f3ff212b9d2a40ed7f0fc0476d726dddc18b1af
daemonica <dima62131@aol.com>
2019-03-14 16:14:30 +0300

    z fixed

--- asdasd.py
+++ asdasd.py
@@ -1,14 +1,11 @@
 import math
-import datas
 
 
-def diff_timeline(otrezok, start, rez):
+def diff_timeline(otrezok):
     x = otrezok
     out = x
     for i in range(0, len(otrezok)-1):
         x[i][1] = math.fabs(math.log(otrezok[i+1][1]) - math.log(otrezok[i][1]))
-    temp = otrezok[-1][1]
-    x[-1][1] = math.fabs(math.log(datas.sequence(rez, start)[1][0][1]) - math.log(temp))
     sum_of_x = 0
     for i in range(0, len(x)):
         sum_of_x += float(x[i][1])
--- main.py
+++ main.py
@@ -16,10 +16,8 @@ rez = datas.filling_empty_days(results, start, end)
 #  print(rez)
 
 rez = datas.floating_mass(rez)
-
 new_rez = datas.sequence(rez, start)
-
-# out = asdasd.diff_timeline(new_rez[0], start, rez)
+out = asdasd.diff_timeline(rez)
 # TODO ПОЧЕМУ ПОСЛЕ ВЫПОЛНЕНИЯ ЭТОЙ ФУНКЦИИ В new_rez НА ПЕРВОМ ИНТЕРВАЛЕ ДИФФЕРЕНЦИРОВАННЫЕ ЗНАЧЕНИЯ???????
 
 # graphs.plot(new_rez[0])
@@ -28,7 +26,6 @@ summ = 0
 for i in range(len(rez)):
     summ += rez[i][1]
 summ /= len(rez)  # среднее арифметическое ВСЕХ данных (всех отрезков)
-print('summ=', summ)
 summ_i = 0
 square_temp = 0
 z = []
@@ -43,15 +40,13 @@ for each in range(len(new_rez)):
         z.append(summ - summ_i)
     summ_i = 0
 
-print('huynya = ', square_temp)
-print('z = ', z)
-print('summ z = ', math.fsum(z))
 s = math.sqrt(square_temp/len(new_rez))
-print('s = ', s)
 alpha = 1.5708  # по той формуле можно альфу на 0.5 поменять и тогда h=0.4011
 r = max(z) - min(z)
-print('R = ', r)
+h = math.log(r/s)/math.log(0.5*alpha*len(new_rez))
 
-h = math.log(r/s)/math.log(alpha*len(new_rez))
+# print('H = ', h)
 
-print('H = ', h)
+k = 1.3 + h
+
+# print('k = ', k)
